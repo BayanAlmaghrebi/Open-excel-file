102953e11152e2fa7365530a0ebd7b1b48809d27
Bayan Almaghrebi <bayan-alm@hotmail.com>
2023-08-14 20:35:43 +0200

    open excel file

---
+++ openexel.py
@@ -0,0 +1,372 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+===== RESTART: C:/Users/bayan/Desktop/my codes/Neuer Ordner/openexelfile.py ====
+OrderDate
+Region
+Manager
+SalesMan
+Item
+Units
+Unit_price
+Sale_amt
+2018-01-06 00:00:00
+East
+Martha
+Alexander
+Television
+95
+1198
+=F2*G2
+2018-01-23 00:00:00
+Central
+Hermann
+Shelli
+Home Theater
+50
+500
+=F3*G3
+2018-02-09 00:00:00
+Central
+Hermann
+Luis
+Television
+36
+1198
+=F4*G4
+2018-02-26 00:00:00
+Central
+Timothy
+David
+Cell Phone
+27
+225
+=F5*G5
+2018-03-15 00:00:00
+West
+Timothy
+Stephen
+Television
+56
+1198
+=F6*G6
+2018-04-01 00:00:00
+East
+Martha
+Alexander
+Home Theater
+60
+500
+=F7*G7
+2018-04-18 00:00:00
+Central
+Martha
+Steven
+Television
+75
+1198
+=F8*G8
+2018-05-05 00:00:00
+Central
+Hermann
+Luis
+Television
+90
+1198
+=F9*G9
+2018-05-22 00:00:00
+West
+Douglas
+Michael
+Television
+32
+1198
+=F10*G10
+2018-06-08 00:00:00
+East
+Martha
+Alexander
+Home Theater
+60
+500
+=F11*G11
+2018-06-25 00:00:00
+Central
+Hermann
+Sigal
+Television
+90
+1198
+=F12*G12
+2018-07-12 00:00:00
+East
+Martha
+Diana
+Home Theater
+29
+500
+=F13*G13
+2018-07-29 00:00:00
+East
+Douglas
+Karen
+Home Theater
+81
+500
+=F14*G14
+2018-08-15 00:00:00
+East
+Martha
+Alexander
+Television
+35
+1198
+=F15*G15
+2018-09-01 00:00:00
+Central
+Douglas
+John
+Desk
+2
+125
+=F16*G16
+2018-09-18 00:00:00
+East
+Martha
+Alexander
+Video Games
+16
+58.5
+=F17*G17
+2018-10-05 00:00:00
+Central
+Hermann
+Sigal
+Home Theater
+28
+500
+=F18*G18
+2018-10-22 00:00:00
+East
+Martha
+Alexander
+Cell Phone
+64
+225
+=F19*G19
+2018-11-08 00:00:00
+East
+Douglas
+Karen
+Cell Phone
+15
+225
+=F20*G20
+2018-11-25 00:00:00
+Central
+Hermann
+Shelli
+Video Games
+96
+58.5
+=F21*G21
+2018-12-12 00:00:00
+Central
+Douglas
+John
+Television
+67
+1198
+=F22*G22
+2018-12-29 00:00:00
+East
+Douglas
+Karen
+Video Games
+74
+58.5
+=F23*G23
+2019-01-15 00:00:00
+Central
+Timothy
+David
+Home Theater
+46
+500
+=F24*G24
+2019-02-01 00:00:00
+Central
+Douglas
+John
+Home Theater
+87
+500
+=F25*G25
+2019-02-18 00:00:00
+East
+Martha
+Alexander
+Home Theater
+4
+500
+=F26*G26
+2019-03-07 00:00:00
+West
+Timothy
+Stephen
+Home Theater
+7
+500
+=F27*G27
+2019-03-24 00:00:00
+Central
+Hermann
+Luis
+Video Games
+50
+58.5
+=F28*G28
+2019-04-10 00:00:00
+Central
+Martha
+Steven
+Television
+66
+1198
+=F29*G29
+2019-04-27 00:00:00
+East
+Martha
+Diana
+Cell Phone
+96
+225
+=F30*G30
+2019-05-14 00:00:00
+Central
+Timothy
+David
+Television
+53
+1198
+=F31*G31
+2019-05-31 00:00:00
+Central
+Timothy
+David
+Home Theater
+80
+500
+=F32*G32
+2019-06-17 00:00:00
+Central
+Hermann
+Shelli
+Desk
+5
+125
+=F33*G33
+2019-07-04 00:00:00
+East
+Martha
+Alexander
+Video Games
+62
+58.5
+=F34*G34
+2019-07-21 00:00:00
+Central
+Hermann
+Sigal
+Video Games
+55
+58.5
+=F35*G35
+2019-08-07 00:00:00
+Central
+Hermann
+Shelli
+Video Games
+42
+58.5
+=F36*G36
+2019-08-24 00:00:00
+West
+Timothy
+Stephen
+Desk
+3
+125
+=F37*G37
+2019-09-10 00:00:00
+Central
+Timothy
+David
+Television
+7
+1198
+=F38*G38
+2019-09-27 00:00:00
+West
+Timothy
+Stephen
+Cell Phone
+76
+225
+=F39*G39
+2019-10-14 00:00:00
+West
+Douglas
+Michael
+Home Theater
+57
+500
+=F40*G40
+2019-10-31 00:00:00
+Central
+Martha
+Steven
+Television
+14
+1198
+=F41*G41
+2019-11-17 00:00:00
+Central
+Hermann
+Luis
+Home Theater
+11
+500
+=F42*G42
+2019-12-04 00:00:00
+Central
+Hermann
+Luis
+Home Theater
+94
+500
+=F43*G43
+2019-12-21 00:00:00
+Central
+Martha
+Steven
+Home Theater
+28
+500
+=F44*G44
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
+None
